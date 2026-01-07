5bcdfbb02bee7b22397b42adea70f047672efbf7
dev-jklagile <jon@jklagile.com>
2026-01-07 22:27:25 +0000

    Add simple counting loop

--- Main.java
+++ Main.java
@@ -1,13 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("What’s your name? ");
-        String name = scanner.nextLine();
-
-        System.out.println("Nice to meet you, " + name + "!");
-        scanner.close();
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
     }
 }
